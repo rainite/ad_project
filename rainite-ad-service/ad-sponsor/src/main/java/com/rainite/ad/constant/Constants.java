bb28241be3a3f979ee128e198d0c7f3157dc3eb9
rainite <kevinchenlife@gmail.com>
2019-07-28 17:23:33 -0400

    added ad plan service implementation

--- rainite-ad-service/ad-sponsor/src/main/java/com/rainite/ad/constant/Constants.java
+++ rainite-ad-service/ad-sponsor/src/main/java/com/rainite/ad/constant/Constants.java
@@ -5,5 +5,7 @@ public class Constants {
     public static class ErrorMsg {
         public static final String REQUEST_PARAM_ERROR = "Request parameter error";
         public static final String SAME_NAME_ERROR = "The name has been registered";
+        public static final String CAN_NOT_FIND_RECORD = "Can not find the data in database";
+        public static final String SAME_NAME_PLAN_ERROR = "Same plan name existed";
     }
 }
---
+++ rainite-ad-service/ad-sponsor/src/main/java/com/rainite/ad/service/impl/AdPlanServiceImpl.java
@@ -0,0 +1,136 @@
+package com.rainite.ad.service.impl;
+
+import com.rainite.ad.constant.CommonStatus;
+import com.rainite.ad.constant.Constants;
+import com.rainite.ad.dao.AdPlanRepository;
+import com.rainite.ad.dao.AdUserRepository;
+import com.rainite.ad.entity.AdPlan;
+import com.rainite.ad.entity.AdUser;
+import com.rainite.ad.exception.AdException;
+import com.rainite.ad.service.IAdPlanService;
+import com.rainite.ad.utils.CommonUtils;
+import com.rainite.ad.vo.AdPlanGetRequest;
+import com.rainite.ad.vo.AdPlanRequest;
+import com.rainite.ad.vo.AdPlanResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AdPlanServiceImpl implements IAdPlanService {
+
+    private final AdUserRepository userRepository;
+    private final AdPlanRepository planRepository;
+
+    @Autowired
+    public AdPlanServiceImpl(AdUserRepository userRepository,
+                             AdPlanRepository planRepository) {
+        this.userRepository = userRepository;
+        this.planRepository = planRepository;
+    }
+
+
+    @Override
+    @Transactional
+    public AdPlanResponse createAdPlan(AdPlanRequest request) throws AdException {
+
+        if(!request.createValidate()){
+            throw new AdException(Constants.ErrorMsg.REQUEST_PARAM_ERROR);
+        }
+
+        // 确保关联的 user 是存在的
+        Optional<AdUser> adUser = userRepository.findById(request.getUserId());
+
+        if (!adUser.isPresent()){
+            throw new AdException(Constants.ErrorMsg.CAN_NOT_FIND_RECORD);
+        }
+
+        AdPlan oldPlan = planRepository.findByUserIdAndPlanName(
+                request.getUserId(), request.getPlanName()
+        );
+
+        if (oldPlan != null){
+            throw new AdException(Constants.ErrorMsg.SAME_NAME_PLAN_ERROR);
+        }
+
+        AdPlan newAdPlan = planRepository.save(
+                new AdPlan(request.getUserId(),request.getPlanName(),
+                        CommonUtils.parseStringDate(request.getStartDate()),
+                        CommonUtils.parseStringDate(request.getEndDate())
+                )
+        );
+
+        return new AdPlanResponse(newAdPlan.getId(), newAdPlan.getPlanName());
+    }
+
+    @Override
+    public List<AdPlan> getAdPlanByIds(AdPlanGetRequest request) throws AdException {
+
+        if (!request.validate()){
+            throw new AdException(Constants.ErrorMsg.REQUEST_PARAM_ERROR);
+        }
+
+        return planRepository.findAllByIdInAndUserId(
+                request.getIds(), request.getUserId()
+        );
+    }
+
+    @Override
+    @Transactional
+    public AdPlanResponse updateAdPlan(AdPlanRequest request) throws AdException {
+
+        if (!request.updateValidate()){
+            throw new AdException(Constants.ErrorMsg.REQUEST_PARAM_ERROR);
+        }
+
+        AdPlan plan = planRepository.findByIdAndUserId(
+                request.getId(), request.getUserId()
+        );
+
+        if (plan == null) {
+            throw new AdException(Constants.ErrorMsg.CAN_NOT_FIND_RECORD);
+        }
+
+        if (request.getPlanName() != null) {
+            plan.setPlanName(request.getPlanName());
+        }
+
+        if (request.getStartDate() != null) {
+            plan.setStartDate(CommonUtils.parseStringDate(request.getStartDate()));
+        }
+
+        if (request.getEndDate() != null) {
+            plan.setEndDate(CommonUtils.parseStringDate(request.getEndDate()));
+        }
+
+        plan.setUpdateTime(new Date());
+        plan = planRepository.save(plan);
+
+        return new AdPlanResponse(plan.getId(), plan.getPlanName());
+    }
+
+    @Override
+    @Transactional
+    public void deleteAdPlan(AdPlanRequest request) throws AdException {
+
+        if (!request.deleteValidate()) {
+            throw new AdException(Constants.ErrorMsg.REQUEST_PARAM_ERROR);
+        }
+
+        AdPlan plan = planRepository.findByIdAndUserId(
+                request.getUserId(), request.getUserId()
+        );
+
+        if (plan == null) {
+            throw new AdException(Constants.ErrorMsg.CAN_NOT_FIND_RECORD);
+        }
+
+        plan.setPlanStatus(CommonStatus.INVALID.getStatus());
+        plan.setUpdateTime(new Date());
+        planRepository.save(plan);
+    }
+}
